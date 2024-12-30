a1a147ef60575a6ba6945e793eb9c74e2b62f1e8
AceLikesGhosts <acelikesghosts@gmail.com>
2024-12-30 08:36:23 -0800

    chore: styling and bug fixing (still proto)

--- src/plugins/_core/settings.tsx
+++ src/plugins/_core/settings.tsx
@@ -5,6 +5,8 @@ import { React } from '@/webpack/common/React';
 import CSSManager from '@/utils/css';
 import { PluginStatus, start, status, toggle, type IPlugin } from '../api';
 import plugins from '~plugins';
+import Logger from '@/utils/logger';
+import { SETTINGS_STORE } from '@/utils/settingsStore';
 
 let SettingsContext: React.Context<{
     settingsShown: boolean;
@@ -13,6 +15,7 @@ let SettingsContext: React.Context<{
 
 let SettingsProvider: ({ children }: { children: React.ReactNode; }) => React.ReactNode;
 
+const logger = Logger.new('settings');
 const patcher = Patcher.new('settings');
 const css = CSSManager.new('settings');
 
@@ -82,7 +85,7 @@ function SettingsPage() {
     }
 
     return (
-        <div className="settings-flex">
+        <div className="settings-flex settings-full-width">
             <div>
                 <span>
                     <a href='#settings' onClick={((e) => { location.hash = '#settings'; location.reload(); })}>
@@ -95,7 +98,7 @@ function SettingsPage() {
                 </span>
             </div>
 
-            <div>
+            <div className='settings-flex-no-center'>
                 <h1>Plugins</h1>
 
                 <ul style={{ listStyle: 'none' }}>
@@ -105,7 +108,7 @@ function SettingsPage() {
                 </ul>
             </div>
 
-            <div>
+            <div className='settings-flex-no-center'>
                 <h2>Required Plugins</h2>
 
                 <ul style={{ listStyle: 'none' }}>
@@ -123,28 +126,28 @@ function PluginCard({
     appendReloadReason
 }: { plugin: IPlugin; appendReloadReason: ({ currentStatus, pluginName }: { currentStatus: PluginStatus; pluginName: string; }) => unknown; }) {
     const [pluginStatus, setPluginStatus] = React.useState<PluginStatus | null>(null);
+    const hasSettings = !SETTINGS_STORE.store[plugin.name];
 
     React.useEffect(() => {
         async function getStatus() {
             const currentPluginStatus = await status(plugin.name);
-            console.log('fetched status', currentPluginStatus);
+            logger.verbose('fetched status', currentPluginStatus);
             setPluginStatus(currentPluginStatus);
-            console.log('set status', currentPluginStatus);
+            logger.verbose('set status', currentPluginStatus);
         }
 
-        if(!pluginStatus) {
-            console.log('plugin status doesnt exist');
+        if(pluginStatus === null) {
+            logger.log('plugin status doesnt exist');
             getStatus();
         }
     }, []);
-
-    console.log('before ret status', pluginStatus);
+    logger.verbose('before ret status', pluginStatus);
 
     return (
         <div className='settings-text-colour'>
             <div className='settings-flex-row'>
                 <h1>{plugin.name}</h1>
-                <span className='settings-text-secondary-colour'>v{plugin.version}</span>
+                <span className='settings-text-secondary-colour' style={{ marginLeft: '4px', marginRight: '4px' }}>(v{plugin.version})</span>
                 <input
                     type="checkbox"
                     name={`${ plugin.name } Toggle`}
@@ -162,7 +165,7 @@ function PluginCard({
                         setPluginStatus(newStatus);
 
                         if(newStatus === PluginStatus.CRASHED) {
-                            console.error('Plugin crashed upon settings page start', plugin.name, newStatus);
+                            logger.error('Plugin crashed upon settings page start', plugin.name, newStatus);
                         }
 
                         appendReloadReason({
@@ -174,7 +177,49 @@ function PluginCard({
             </div>
 
             <div>
-                {plugin.authors && plugin.authors.length > 0 && <span>by {plugin.authors?.map((authors) => authors).join(', ')}</span>}
+                <button
+                    type='button'
+                    style={{
+                        width: '36px',
+                        height: '36px',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center'
+                    }}
+                    disabled={hasSettings}
+                >
+                    <svg
+                        stroke="currentColor"
+                        fill="currentColor"
+                        stroke-width="0"
+                        viewBox="0 0 512 512"
+                        xmlns="http://www.w3.org/2000/svg"
+
+                        height={'18px'}
+                        width={'18px'}
+
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center'
+                        }}
+                    >
+                        <path
+                            d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z">
+                        </path>
+                    </svg>
+                </button>
+            </div>
+
+            <div>
+                {plugin.description}
+            </div>
+
+            <div>
+                {plugin.authors && plugin.authors.length > 0
+                    // && <span>by {plugin.authors?.map((authors) => authors).join(', ')}</span>
+                    && <span>by {plugin.authors.map((author) => <a href={author.githubUrl} target='_blank'>{author.shortName}</a>)}</span>
+                }
             </div>
         </div>
     );
@@ -185,17 +230,13 @@ export default definePlugin({
     version: '1.0.0',
     isRequired: true,
     settingsButton: ({ original }: { original: React.FunctionComponent; }) => {
-        console.log('orig', original);
-
         if(!SettingsContext) {
-            console.log('settingsButton no SettingsContext')
+            logger.log('settingsButton no SettingsContext');
             return snpext.webpack.common.React.createElement(original, {}, []);
         }
 
-
-        console.log('settingContext', SettingsContext);
         const settingsContext = React.useContext(SettingsContext);
-        console.log('settingsContext~settingsButton', settingsContext.settingsShown);
+        logger.verbose('settingsContext~settingsButton', settingsContext.settingsShown);
 
         return (
             <div style={{
@@ -210,9 +251,8 @@ export default definePlugin({
                     display: 'flex',
                 }}
                     onClick={(() => {
-                        // open settings page somehow
                         settingsContext.setSettingsShown(!settingsContext.settingsShown);
-                        console.log('on click called', settingsContext.settingsShown);
+                        logger.verbose('on click called', settingsContext.settingsShown);
                     })}
                 >
                     <button
@@ -252,10 +292,10 @@ export default definePlugin({
     },
 
     intermediateSettingsComponent: ({ original, args }: { original: React.FunctionComponent; args: Record<PropertyKey, unknown> | undefined, }) => {
-        console.log('intermediateSettingsComponent called');
+        logger.verbose('intermediateSettingsComponent called');
 
         if(!SettingsContext) {
-            console.log('intermediateSettingsComponent failed to find settings context');
+            logger.log('intermediateSettingsComponent failed to find settings context');
             return snpext.webpack.common.React.createElement(original, args ?? {}, []);
         }
 
@@ -280,6 +320,12 @@ export default definePlugin({
         }
 
         css.insert(`
+        .settings-full-width {
+            width: 100%
+            margin-left: 10px;
+            margin-right: 10px;
+        }
+
         .settings-flex {
             display: flex;
             flex-direction: column;
@@ -289,6 +335,13 @@ export default definePlugin({
             width: 100%;
 
             // border-left: solid #2e2d2d 2px;
+        }
+
+        .settings-flex-no-center {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            width: 100%;
         }
 
         .settings-flex-row {
@@ -330,7 +383,7 @@ export default definePlugin({
         };
 
         patcher.after(obj, key, (args, result, ctx) => {
-            console.log('patched zne called', args, result, ctx);
+            logger.log('patched zne called', args, result, ctx);
             return (
                 <SettingsProvider>
                     {result}
