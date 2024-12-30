0b03796a551de693edceef67dbcddeafb324a9c0
AceLikesGhosts <acelikesghosts@gmail.com>
2024-12-30 08:35:00 -0800

    temp-fix: only override properties on objects if they are functions
    this is a temporary fix in order to prevent some weird state issue where snapchat is abusing readonly properties and setting them to null etc when they shouldnt be.. but it doesnt break prod normally because theyre readonly... wtf?

--- src/entry.ts
+++ src/entry.ts
@@ -6,40 +6,21 @@ Object.freeze(console); // FUCK YOU SNAPCHAT!
 // that webpack exports elements using `defineProperty`
 // with only a getter, which makes it impossible to
 // patch them normally, (i.e. patching the main React entrypoint)
+// this *FOR NOW* only patches functions allowing for replacing
+// them via the patcher
 
 // this was added 08/12/24
 // for the settings core plugin
 new Patcher('internal')
     .instead(Object, 'defineProperty', (
-        [object, propertyKey, attributes],
-        original,
-        context
+        [obj, prop, descriptor],
+        original
     ) => {
-        const newAttributes = { ...attributes };
-
-        // Check if we have a getter and no setter
-        if(newAttributes.get && !newAttributes.set) {
-            // Create a private variable to hold the value
-            const value = { current: undefined };
-
-            // Define a new setter that updates the private value
-            newAttributes.set = (v) => {
-                value.current = v;
-            };
-
-            // Define a new getter that returns the private value
-            newAttributes.get = () => {
-                return value.current || (value.current = attributes.get?.bind(context).call(object));
-            };
+        if(typeof descriptor === 'object' && descriptor !== null && typeof descriptor.value === 'function') {
+            // Only modify the descriptor if a function is being defined
+            return original(obj, prop, { ...descriptor, configurable: true });
         }
-
-        // Ensure we don't mix value/writable with get/set
-        if(newAttributes.get || newAttributes.set) {
-            delete newAttributes.value;
-            delete newAttributes.writable;
-        }
-
-        return original(object, propertyKey, newAttributes);
+        return original(obj, prop, descriptor);
     });
 
 export * as storage from './utils/storage';
