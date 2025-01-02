66b404a30a5b2b29a6b0999a69988bfec21f879e
AceLikesGhosts <acelikesghosts@gmail.com>
2025-01-01 18:06:03 -0800

    cleanup again?

--- src/entry.ts
+++ src/entry.ts
@@ -16,36 +16,13 @@ new Patcher('internal')
         [obj, prop, descriptor],
         original
     ) => {
-        // if(typeof descriptor.get === 'function' && typeof descriptor.set === 'undefined') {
-        //     if(descriptor.hasOwnProperty('value') || descriptor.hasOwnProperty('writable')) {
-        //         throw new TypeError('Cannot specify both accessors and a value/writable attribute.');
-        //     }
-
-        //     descriptor.configurable = true;
-
-        //     // descriptor.value = descriptor.get;
-        //     // descriptor.get = () => descriptor.value
-        //     let internalValue;
-
-        //     descriptor.get = function() {
-        //         return internalValue || descriptor.get?.bind(obj)
-        //     }
-
-        //     descriptor.set = descriptor.set || function (newValue: any) {
-        //         console.log(`Setter called for getter-based function: ${ prop }`);
-        //         internalValue = newValue;
-        //     };
-        // }
-
         if(descriptor.get && !descriptor.set) {
             descriptor.configurable = true;
 
             let iv = descriptor.get.bind(obj);
-            // delete descriptor.get;
 
             descriptor.get = iv;
             descriptor.set = (v) => iv = v;
-            // descriptor.value = iv.bind(obj);
         }
 
         return original(obj, prop, descriptor);
