3f4c2480ceda09731536d0490c0fe2fb579371d3
AceLikesGhosts <acelikesghosts@gmail.com>
2025-04-20 04:22:56 -0700

    chore: stop hardcoding store names

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -2,27 +2,31 @@ import { openDB, type IDBPDatabase } from 'idb';
 import pkg from './pkg';
 import type { PluginStatus } from '@/plugins/api';
 
-const idbDb: Promise<IDBPDatabase<unknown>> = openDB(pkg.name, 1, {
+export const IDB_SETTINGS_KEY = 'settings';
+export const IDB_PLUGIN_STATUS_KEY = 'pluginStatus';
+
+/* @internal */
+export const idbDb: Promise<IDBPDatabase<unknown>> = openDB(pkg.name, 1, {
     upgrade(db, oldversion) {
         if(oldversion === 0) {
-            db.createObjectStore('settings');
-            db.createObjectStore('pluginStatus');
+            db.createObjectStore(IDB_SETTINGS_KEY);
+            db.createObjectStore(IDB_PLUGIN_STATUS_KEY);
         }
     },
 });
 
 export const setPluginStatus = async (key: string, status: PluginStatus) => {
-    await (await idbDb).put('pluginStatus', status, key);
+    await (await idbDb).put(IDB_PLUGIN_STATUS_KEY, status, key);
 };
 
 export const getPluginStatus = async (key: string): Promise<PluginStatus | undefined> => {
-    return (await idbDb).get('pluginStatus', key);
+    return (await idbDb).get(IDB_PLUGIN_STATUS_KEY, key);
 };
 
 export const get = async <T>(key: string): Promise<T | undefined> => {
-    return (await idbDb).get('settings', key);
+    return (await idbDb).get(IDB_SETTINGS_KEY, key);
 };
 
 export const set = async (key: string, value: unknown) => {
-    await (await idbDb).put('settings', value, key);
+    await (await idbDb).put(IDB_SETTINGS_KEY, value, key);
 };
